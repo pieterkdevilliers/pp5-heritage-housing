49177783a0f7abdfa80c728615cb49aa5a1e860a
Pieter K de Villiers <pieter@hey.com>
2023-06-20 20:44:48 +0100

    Add Project Hypothesis page content to dashboard

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 from app_pages.multi_page import MultiPage
 
-from app_pages import house_price_data_exploration, house_price_estimator, hypothesis_outline, project_summary
+from app_pages import house_price_data_exploration, house_price_estimator, project_hypothesis, project_summary
 
 app = MultiPage(app_name= "House Price Estimator") # Create an instance of the app 
 
 app.add_page("Project Summary", project_summary.project_summary) # Add the project summary page
-app.add_page("Hypothesis Outline", hypothesis_outline.hypothesis_outline) # Add the hypothesis outline page
+app.add_page("Hypothesis Outline", project_hypothesis.project_hypothesis) # Add the project hypothesis outline page
 app.add_page("House Price Data Exploration", house_price_data_exploration.house_price_data_exploration) # Add the house price data exploration page
 app.add_page("House Price Estimator", house_price_estimator.house_price_estimator) # Add the house price estimator page
 
---
+++ app_pages/project_hypothesis.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+def project_hypothesis():
+    st.write("### Project Hypothesis")
+
+    # information from README file
+    st.success(
+        f"**Project Hypothesis**\n"
+        f"* The sale price of a property is assumed to be correlated with the features of the house.\n"
+        f" However, the hypothesis here is that not all features are created equal. Some features will be much more closely correlated with the sale price of a property.\n"
+        f" In order to estimate the sale price of a property,\n"
+        f"we need to determine which of the property features correlated more closely to the sale price.\n"
+        f"In this project we are using Spearman and Pearson correlation to study the feature correlation to the sale price.\n"
+        f"We are using Predictive Power Score to determine the features of a property most closely correlated to the sale price.\n")
+    
+    st.write("### Feature Importance Results")
+    st.write(
+        f"This image shows the result of the feature importance analysis,\n"
+        f"with Overall Quality and Ground Floor Living Area proving to be the stringest predictors of sale price.\n")
+    st.image("outputs/predict_price/v1/features_importance.png", width=600)
