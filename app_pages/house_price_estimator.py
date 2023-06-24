4a43ef9f0b2515111e8a7a4cbd31369b07e7124e
Pieter K de Villiers <pieter@hey.com>
2023-06-24 09:58:51 +0100

    Add inherited house details and predict inherited house sale prices - resolve joblib version issue

--- app_pages/house_price_estimator.py
+++ app_pages/house_price_estimator.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import joblib
 from src.data_loading import load_house_price_data, load_pkl_file
 
 def house_price_estimator():
@@ -8,12 +9,32 @@ def house_price_estimator():
 
     # load inherited houses file
     df = pd.read_csv("inputs/datasets/raw/inherited_houses/house-prices/inherited_houses.csv")
+
+    st.write(
+            f"* The dataset has {df.shape[0]} rows and {df.shape[1]} columns, "
+            f"displaying the data for the inherited houses.")
+        
+    st.write(df)
 	
     # load files needed for predicting house prices 
     version = 'v1'
-    print(version)
-    pipeline = load_pkl_file(f"outputs/predict_price/{version}/best_regressor_pipeline.pkl")
+    pipeline = joblib.load(f"outputs/predict_price/{version}/best_regressor_pipeline.pkl")
     best_features = (pd.read_csv(f"outputs/predict_price/{version}/X_train.csv")
                             # .columns
                             # .to_list()
                             )
+    
+        # predict prices of inherited houses with ML pipeline	
+    st.write("### Details of the inherited houses and their predicted sale prices")
+    st.write(
+        f"* Details of the inherited houses, showing all features."
+    )
+    st.write(df)
+    df = df.filter(best_features)
+    house_price_prediction = pipeline.predict(df).round(0)
+    df['Predicted House Sale Price'] = house_price_prediction
+    st.write(
+        f"* This table shows the predicted sale prices for the inherited houses, focussing on the identified features with the strongest PPS: "
+        "'Overall Quality' and 'Above Ground Living Area Square Feet'."
+    )
+    st.write(df)
