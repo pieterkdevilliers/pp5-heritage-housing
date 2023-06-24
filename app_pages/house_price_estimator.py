5db21a1e2950622513efbbf007cbf95dddfcda4b
Pieter K de Villiers <pieter@hey.com>
2023-06-24 09:12:24 +0100

    Start house_price_estimator page

--- app_pages/house_price_estimator.py
+++ app_pages/house_price_estimator.py
@@ -1,4 +1,19 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+from src.data_loading import load_house_price_data, load_pkl_file
 
 def house_price_estimator():
     st.write("This is the house price estimator page.")
+
+    # load inherited houses file
+    df = pd.read_csv("inputs/datasets/raw/inherited_houses/house-prices/inherited_houses.csv")
+	
+    # load files needed for predicting house prices 
+    version = 'v1'
+    print(version)
+    pipeline = load_pkl_file(f"outputs/predict_price/{version}/best_regressor_pipeline.pkl")
+    best_features = (pd.read_csv(f"outputs/predict_price/{version}/X_train.csv")
+                            # .columns
+                            # .to_list()
+                            )
--- src/data_loading.py
+++ src/data_loading.py
@@ -9,3 +9,7 @@ def load_house_price_data():
     df = pd.read_csv("outputs/datasets/collection/house_prices_records.csv")
     return df
 
+# load pkl file
+def load_pkl_file(file_path):
+    return joblib.load(filename=file_path)
+
